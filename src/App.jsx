e3220b017d875c34de944a1c52211699579b306a
UMAIR9235 <umairslm80@gmail.com>
2023-10-29 13:15:02 +0530

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Footer from "./layout/footer/Footer";
 import Header from "./layout/header/Header";
 import Main from "./layout/main/Main";
-import Cta from "./layout/cta/cta";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <BrowserRouter>
         <Header />
         <Main />
-        <Cta />
         <Footer />
       </BrowserRouter>
     </div>
--- src/layout/main/Main.jsx
+++ src/layout/main/Main.jsx
@@ -1,10 +1,12 @@
 import Routers from "../../routers/Routers";
+import Cta from "../../components/main/cta/cta";
 
 import "./Main.css";
 const Main = function () {
   return (
     <div className="main-container">
       <Routers />
+      <Cta />
     </div>
   );
 };
